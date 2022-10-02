1de94978b0c8547b8bb3050ce1e0d14a51c70e3e
Greg <greg@toki.waseda.jp>
2022-10-03 03:16:55 +0900

    fix hostcalls

--- example/hostcall.mjs
+++ example/hostcall.mjs
@@ -8,5 +8,10 @@ const pl = new Prolog();
 console.dir(
 	await pl.queryOnce(`time(js_eval_json("return console.log('hello from js'), {\\"hello\\":\\"abc\\"}", X)).`, {format: "prolog"}), {depth: null});
 
-console.dir(
-	await pl.queryOnce(`js_eval_json("return new Date()", X).`, {format: "prolog"}), {depth: null});
+console.log(
+	await pl.queryOnce(`fetch("https://httpbin.org/get", Content).`, {program: `
+	:- use_module(library(format)).
+	fetch(URL, Content) :-
+	  phrase(format_("return fetch(~w).then(x => x.text());", [URL]), JS),
+	  js_eval_json(JS, X), write(X).
+	`, format: "prolog"}), {depth: null});
