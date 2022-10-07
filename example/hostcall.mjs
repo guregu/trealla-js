5df8963c232a66fc798136159ab4e3582766f0f5
Greg <greg@toki.waseda.jp>
2022-10-07 16:54:22 +0900

    add js_fetch/3, http_consult/1

--- example/hostcall.mjs
+++ example/hostcall.mjs
@@ -15,3 +15,11 @@ console.log(
 	  phrase(format_("return fetch(~w).then(x => x.text());", [URL]), JS),
 	  js_eval_json(JS, X), write(X).
 	`, format: "prolog"}), {depth: null});
+
+console.dir(
+	await pl.queryOnce(`js_fetch("https://httpbin.org/post", Content, [as(json), body({"hello": "world"}), method(post), headers([foo-bar])]), write(Content), nl.`, {format: "prolog"}),
+	{depth: null});
+
+console.dir(
+	await pl.queryOnce(`http_consult("https://raw.githubusercontent.com/guregu/worker-prolog/978c956801ffff83f190450e5c0325a9d34b064a/src/views/examples/fizzbuzz.pl"), fizzbuzz(1, 21).`),
+	{depth: null});
