c64f9be4a0a1faf588a4c724252ad17879d67f7a
Greg <greg@toki.waseda.jp>
2022-10-11 02:04:40 +0900

    update trealla

--- example/hostcall.mjs
+++ example/hostcall.mjs
@@ -7,12 +7,12 @@ const pl = new Prolog();
 console.dir(
 	await pl.queryOnce(`time(js_eval_json("return console.log('hello from js'), {\\"hello\\":\\"abc\\"}", X)).`, {format: "prolog"}), {depth: null});
 
-console.log(
+console.dir(
 	await pl.queryOnce(`fetch("https://httpbin.org/get", Content).`, {program: `
 	:- use_module(library(format)).
 	fetch(URL, Content) :-
 	  phrase(format_("return fetch(~w).then(x => x.text());", [URL]), JS),
-	  js_eval_json(JS, X), write(X).
+	  js_eval_json(JS, Content), write(Content).
 	`, format: "prolog"}), {depth: null});
 
 console.dir(
