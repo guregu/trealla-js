f516bb0fee5828c1eef5f21a5afc8c873622d3f5
Greg <greg@toki.waseda.jp>
2022-10-20 13:35:21 +0900

    update trealla, adds crypto_data_hash/3

--- example/hostcall.mjs
+++ example/hostcall.mjs
@@ -1,5 +1,10 @@
 import { load, Prolog } from '../trealla.js';
 
+// Node needs the global crypto object defined.
+// Exists already in browsers.
+import crypto from 'node:crypto';
+globalThis.crypto = crypto;
+
 await load();
 
 const pl = new Prolog();
@@ -20,5 +25,14 @@ console.dir(
 	{depth: null});
 
 console.dir(
-	await pl.queryOnce(`http_consult("https://raw.githubusercontent.com/guregu/worker-prolog/978c956801ffff83f190450e5c0325a9d34b064a/src/views/examples/fizzbuzz.pl"), fizzbuzz(1, 21).`),
+	await pl.queryOnce(`http_consult("https://raw.githubusercontent.com/guregu/worker-prolog/978c956801ffff83f190450e5c0325a9d34b064a/src/views/examples/fizzbuzz.pl"), fizzbuzz(1, 21).`, {format: "json"}),
 	{depth: null});
+
+for await (const x of pl.query(`between(1,3,N), once(phrase(format_("return ~w;", [N]), JS)), js_eval_json(JS, Got)`, {format: "json"})) {
+	console.log(x);
+}
+for await (const x of pl.query(`between(1,3,N), once(phrase(format_("return new Promise((resolve) => resolve(~w))", [N]), JS)), js_eval_json(JS, Got)`, {format: "json"})) {
+	console.log(x);
+}
+
+console.log(await pl.queryOnce(`crypto_data_hash("foo", Hash, [algorithm(Algo)]).`, {format: "prolog"}));
